21ff1049364b9a31d3b42836eec68d725cfbe950
FUBU <brendonangelo@hotmail.com>
2021-05-20 22:13:36 -0300

    .

--- lib/Usuario/telaUsuario.dart
+++ lib/Usuario/telaUsuario.dart
@@ -1,21 +1,10 @@
 import 'package:flutter/cupertino.dart';
 // import 'package:carousel_slider/carousel_controller.dart';
-import 'package:carousel_slider/carousel_options.dart';
-import 'package:carousel_slider/carousel_slider.dart';
+// import 'package:carousel_slider/carousel_options.dart';
+// import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
-
-final List<Widget> containers = [
-  Container(
-    width: 50,
-    height: 50,
-    color: Colors.black,
-  ),
-  Container(
-    width: 50,
-    height: 50,
-    color: Colors.blue,
-  )
-];
+import 'package:flutter_svg/flutter_svg.dart';
+// import 'dart:collection';
 
 class TelaUsuario extends StatefulWidget {
   @override
@@ -25,22 +14,69 @@ class TelaUsuario extends StatefulWidget {
 class _TelaUsuario extends State<TelaUsuario> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text('Fullscreen sliding carousel demo')),
-      body: Builder(
-        builder: (context) {
-          final double height = MediaQuery.of(context).size.height;
-          return CarouselSlider(
-            options: CarouselOptions(
-              height: height,
-              viewportFraction: 1.0,
-              enlargeCenterPage: false,
-              // autoPlay: false,
-            ),
-            items: containers.map((item) => Container(child: Center()).to()),
-          );
-        },
+    final double height = MediaQuery.of(context).size.height;
+    final double width = MediaQuery.of(context).size.width;
+
+    return Column(children: <Widget>[
+      Flexible(
+        fit: FlexFit.tight,
+        flex: 2,
+        child: Container(
+          color: Colors.blue,
+          child: Row(
+            children: <Widget>[
+              Flexible(
+                flex: 1,
+                child: ButtonTheme(
+                  // height: 60,
+                  // minWidth: 60,
+                  child: FlatButton(
+                    child: SvgPicture.asset(
+                      'images/SplashScreen/fogo_Tinder.svg',
+                      height: 20,
+                    ),
+                    onPressed: () {},
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    );
+      Flexible(
+        flex: 14,
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          // physics: NeverScrollableScrollPhysics(),
+          children: <Widget>[
+            Container(
+              height: height,
+              width: width,
+              color: Colors.red,
+            ),
+            Container(
+              height: height,
+              width: width,
+              color: Colors.blue,
+            ),
+            Container(
+              height: height,
+              width: width,
+              color: Colors.green,
+            ),
+            Container(
+              height: height,
+              width: width,
+              color: Colors.yellow,
+            ),
+            Container(
+              height: height,
+              width: width,
+              color: Colors.orange,
+            ),
+          ],
+        ),
+      ),
+    ]);
   }
 }

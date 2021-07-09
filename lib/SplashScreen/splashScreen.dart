9c4c5cc818d25e80d3b493a0810e0aa7dd990ad2
Brendon Angelo Rodrigues <brendonangelo@hotmail.com>
2021-07-09 05:51:09 -0300

    Mudando o código para usar o Lottie package

---
+++ lib/SplashScreen/splashScreen.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:lottie/lottie.dart';
+import 'package:tinder_app/Login/telaInicial.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  AnimationController _controller;
+
+  changeStatusBar() {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light.copyWith(
+      statusBarColor: Colors.black,
+    ));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    changeStatusBar();
+    _controller = AnimationController(vsync: this);
+    // Future.delayed(Duration(seconds: 3), () {
+    // Navigator.pushReplacement(
+    //     context, MaterialPageRoute(builder: (_) => TelaInicial()));
+    // });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // final size = MediaQuery.of(context).size;
+    return SizedBox.expand(
+      child: FittedBox(
+        fit: BoxFit.cover,
+        child: Lottie.network(
+          "https://assets6.lottiefiles.com/packages/lf20_9mxwdghu.json",
+          fit: BoxFit.fill,
+          controller: _controller,
+          onLoaded: (composition) {
+            _controller
+              ..duration = composition.duration
+              ..forward().whenComplete(
+                () => {
+                  Navigator.pushReplacement(
+                      context, MaterialPageRoute(builder: (_) => TelaInicial()))
+                },
+              );
+          },
+        ),
+      ),
+    );
+  }
+}
